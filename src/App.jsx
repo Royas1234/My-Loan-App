ef35d2047439a1b016ad6b439d68c3a6caf9e9a9
Royas1234 <rosheedatoyindamola10@gmail.com>
2023-08-30 00:41:38 +0100

    set up eslint and prettier with husky

--- src/App.jsx
+++ src/App.jsx
@@ -17,34 +17,40 @@ import NewDashboard from "./Pages/Dashboard/DashboardOverview/NewDashboard";
 import UserPreference from "./Pages/Dashboard/Settings/User-Preference/UserPreference";
 import ContactForm from "./components/NewForm/ContactForm";
 import CreatePersonal from "./Pages/Dashboard/PortfolioAnalysis/Components/NewPortfolio/CreatePersonal/CreatePersonal";
-let name;
-let age;
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about-us" element={<AboutUs />} />
-          <Route exact path="/pricing" element={<Pricing />} />
-          <Route exact path="/blog" element={<Blog />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/forgot-Password" element={<ForgotPassword />} />
-          <Route exact path="/contact-form" element={<ContactForm />} />
-          <Route exact path="/security-question" element={<SecurityQuestion />}/>
-          <Route exact path="/create-account" element={<CreateAccount />} />
-          <Route exact path="/account-verify" element={<AccountVerify />} />
-          <Route exact path="/forget-password-verify" element={<ForgotPasswordVerify />} />
-          <Route exact path="/new-password" element={<NewPassword />} />
-          <Route exact path="/successful" element={<SuccessfulPage />} />
-          <Route path="/dashboard/*" element={<NewDashboard />} />
-          <Route path="/userPreference" element={<UserPreference />} />
-          <Route path="/personal" element={<CreatePersonal />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/about-us" element={<AboutUs />} />
+					<Route exact path="/pricing" element={<Pricing />} />
+					<Route exact path="/blog" element={<Blog />} />
+					<Route exact path="/login" element={<Login />} />
+					<Route exact path="/forgot-Password" element={<ForgotPassword />} />
+					<Route exact path="/contact-form" element={<ContactForm />} />
+					<Route
+						exact
+						path="/security-question"
+						element={<SecurityQuestion />}
+					/>
+					<Route exact path="/create-account" element={<CreateAccount />} />
+					<Route exact path="/account-verify" element={<AccountVerify />} />
+					<Route
+						exact
+						path="/forget-password-verify"
+						element={<ForgotPasswordVerify />}
+					/>
+					<Route exact path="/new-password" element={<NewPassword />} />
+					<Route exact path="/successful" element={<SuccessfulPage />} />
+					<Route path="/dashboard/*" element={<NewDashboard />} />
+					<Route path="/userPreference" element={<UserPreference />} />
+					<Route path="/personal" element={<CreatePersonal />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;

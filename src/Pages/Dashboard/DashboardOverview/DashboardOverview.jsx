9fdcf2e5e7d01bcfb924064b8cceff4352afb610
Royas1234 <rosheedatoyindamola10@gmail.com>
2023-09-12 18:35:40 +0100

    added token to local storage

--- src/Pages/Dashboard/DashboardOverview/DashboardOverview.jsx
+++ src/Pages/Dashboard/DashboardOverview/DashboardOverview.jsx
@@ -2,28 +2,33 @@ import React, { useEffect, useState } from "react";
 import DashSearch from "./components/DashSearch";
 import PaginationTable from "../../../components/Overview/PaginationTable";
 import "./dashboard.css";
-import axios from 'axios'
+import axios from "axios";
 import ChartCards from "./components/ChartCards";
-
 
 const DashboardOverview = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [loanData, setLoanData] = useState([]);
   const [searchItems, setSearchItems] = useState("");
-  
-  // const [selectedCategory, setSelectedCategory] = useState('')
-   const categoryStateArray  = useState('');
-    const setSelectedCategory = categoryStateArray[1]
-     
+
+  const categoryStateArray = useState("");
+  const setSelectedCategory = categoryStateArray[1];
+
   useEffect(() => {
-    axios.get("https://loanwise.onrender.com/api/loan-table")
-      .then(response => {
-        setSearchResults(response.data)
-        setLoanData(response.data)
-        
+    const savedToken = JSON.parse(localStorage.getItem("token"));
+    if (savedToken) {
+      console.log(savedToken);
+    }
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://loanwise.onrender.com/api/loan-table")
+      .then((response) => {
+        setSearchResults(response.data);
+        setLoanData(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error)
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, []);
 
@@ -36,13 +41,10 @@ const DashboardOverview = () => {
     });
 
     setSearchResults(results);
-  
-    
-  }, [searchItems,loanData]);
+  }, [searchItems, loanData]);
 
   const handleSearch = (term) => {
     setSearchItems(term);
-  
   };
 
   const handleFilter = (category) => {
@@ -50,19 +52,20 @@ const DashboardOverview = () => {
       setSearchResults(loanData);
       return;
     }
-    const filteredResults = loanData.filter((user) => user["loan_status"] === category);
+    const filteredResults = loanData.filter(
+      (user) => user["loan_status"] === category
+    );
     setSelectedCategory(category);
-   
+
     setSearchResults(filteredResults);
-  }
-
-
+  };
 
   return (
     <div className="overview-container container">
       <div className="explore">
         <p>
-          Explore insightful analyses and risk assessment to make informed lending decisions.
+          Explore insightful analyses and risk assessment to make informed
+          lending decisions.
         </p>
       </div>
       <div className="overview-search-filter">
@@ -70,11 +73,7 @@ const DashboardOverview = () => {
           <DashSearch handleSearch={handleSearch} handleFilter={handleFilter} />
         </div>
         <ChartCards />
-        <PaginationTable
-          data={searchResults}
-          
-       
-        />
+        <PaginationTable data={searchResults} />
       </div>
     </div>
   );

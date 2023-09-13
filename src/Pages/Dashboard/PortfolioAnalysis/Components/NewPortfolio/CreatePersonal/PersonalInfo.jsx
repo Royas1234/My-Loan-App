d9fa5048b743c3bd047f3b670eb9e2c25c7d8e98
Royas1234 <rosheedatoyindamola10@gmail.com>
2023-09-13 20:37:26 +0100

    no-console error

--- src/Pages/Dashboard/PortfolioAnalysis/Components/NewPortfolio/CreatePersonal/PersonalInfo.jsx
+++ src/Pages/Dashboard/PortfolioAnalysis/Components/NewPortfolio/CreatePersonal/PersonalInfo.jsx
@@ -8,165 +8,157 @@ import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const userSchema = Yup.object().shape({
-  fullName: Yup.string()
-    .required("The name field is required")
-    .min(11, "Must be at least 11 characters"),
-  address: Yup.string()
-    .required("Enter your residential address")
-    .min(11, "Must be more than 11 characters"),
-  email: Yup.string()
-    .email("Invalid email")
-    .required("The email field is required"),
-  phoneNumber: Yup.string()
-    .required("Phone number is required")
-    .matches(
-      /^(\\+[1-9]{1,4}[ \\-]*)?(\\([0-9]{2,3}\\)[ \\-]*)?([0-9]{2,4})[ \\-]*[0-9]{3,4}[ \\-]*[0-9]{3,4}$/,
-      "Invalid phone number format"
-    )
-    .min(10, "Phone number must be at up to 10 characters"),
+ fullName: Yup.string()
+  .required("The name field is required")
+  .min(11, "Must be at least 11 characters"),
+ address: Yup.string()
+  .required("Enter your residential address")
+  .min(11, "Must be more than 11 characters"),
+ email: Yup.string()
+  .email("Invalid email")
+  .required("The email field is required"),
+ phoneNumber: Yup.string()
+  .required("Phone number is required")
+  .matches(
+   /^(\\+[1-9]{1,4}[ \\-]*)?(\\([0-9]{2,3}\\)[ \\-]*)?([0-9]{2,4})[ \\-]*[0-9]{3,4}[ \\-]*[0-9]{3,4}$/,
+   "Invalid phone number format"
+  )
+  .min(10, "Phone number must be at up to 10 characters"),
 
-  alternatePhone: Yup.string()
-    .required("Phone number is required")
-    .matches(
-      /^(\\+[1-9]{1,4}[ \\-]*)?(\\([0-9]{2,3}\\)[ \\-]*)?([0-9]{2,4})[ \\-]*[0-9]{3,4}[ \\-]*[0-9]{3,4}$/,
-      "Invalid phone number format"
-    )
-    .min(10, "Phone number must be at up to 10 characters"),
-  dateOfBirth: Yup.date()
-    .required("This field is required")
-    .typeError("Invalid Date"),
-  bvn: Yup.string().required("Enter Number"),
+ alternatePhone: Yup.string()
+  .required("Phone number is required")
+  .matches(
+   /^(\\+[1-9]{1,4}[ \\-]*)?(\\([0-9]{2,3}\\)[ \\-]*)?([0-9]{2,4})[ \\-]*[0-9]{3,4}[ \\-]*[0-9]{3,4}$/,
+   "Invalid phone number format"
+  )
+  .min(10, "Phone number must be at up to 10 characters"),
+ dateOfBirth: Yup.date()
+  .required("This field is required")
+  .typeError("Invalid Date"),
+ bvn: Yup.string().required("Enter Number"),
 });
 
 const PersonalInfo = ({ handleNext }) => {
-  const [inValid, setInValid] = useState("");
+ const [inValid, setInValid] = useState("");
 
-  /*
-   * Validation
-   */
-  const methods = useForm({
-    resolver: yupResolver(userSchema),
-  });
+ /*
+  * Validation
+  */
+ const methods = useForm({
+  resolver: yupResolver(userSchema),
+ });
 
-  const { handleSubmit, reset } = methods;
+ const { handleSubmit, reset } = methods;
 
-  const onSubmit = async (data) => {
-    const values = {
-      name: data.fullName,
-      email: data.email,
-      address: data.address,
-      phoneNumber: data.phoneNumber,
-      dateOfBirth: data.dateOfBirth,
-      bvn: data.bvn,
-    };
-
-    try {
-      const response = await axios.post(
-        "https://loanwise.onrender.com/api/borrowers-details",
-        values
-      );
-      handleNext();
-      console.log(response.data)
-      console.log("Form submitted successfully");
-      reset();
-    } catch (error) {
-      if (error.response) {
-        console.log("Request failed with status code:", error.response.status);
-        console.log("Response data:", error.response.data);
-        // Set error state and display error message to the user
-        setInValid(error.response.data.message);
-      } else {
-        console.error("Error while submitting form:", error.message);
-      }
-    }
+ const onSubmit = async (data) => {
+  const values = {
+   name: data.fullName,
+   email: data.email,
+   address: data.address,
+   phoneNumber: data.phoneNumber,
+   dateOfBirth: data.dateOfBirth,
+   bvn: data.bvn,
   };
 
-  return (
-    <FormProvider {...methods}>
-      {inValid && (
-        <span style={{ color: "red", marginBottom: "30px" }}>{inValid}</span>
-      )}
-      <form className="new-portfolio-form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="new-portfolio-wrapper">
-          <h2>Personal Information</h2>
-          <div>
-            <NewFormInput
-              autoFocus={true}
-              type="text"
-              label="Full Name"
-              name="fullName"
-              placeholder="Enter full name"
-            />
-          </div>
-          <div>
-            <NewFormInput
-              type="text"
-              label="Residential Address"
-              name="address"
-              placeholder="Enter address"
-            />
-          </div>
-          <div>
-            <NewFormInput
-              name="email"
-              type="email"
-              label="Email Address"
-              placeholder="Enter email address"
-            />
-          </div>
-          <div className="dFlex">
-            <div className="dFlex1">
-              <NewFormInput
-                name="phoneNumber"
-                type="phone"
-                label="Phone Number"
-                placeholder="Enter number"
-              />
-            </div>
-            <div className="dFlex2">
-              <NewFormInput
-                name="alternatePhone"
-                type="phone"
-                label="Alternate Phone Number"
-                placeholder="Enter number"
-              />
-            </div>
-          </div>
-          <div className="dFlex">
-            <div className="dFlex1">
-              <NewFormInput
-                name="dateOfBirth"
-                type="date"
-                label="Date of Birth"
-              />
-            </div>
-            <div className="dFlex2">
-              <NewFormInput
-                name="bvn"
-                type="number"
-                label="Bank Verification Number"
-                placeholder="Enter number"
-              />
-            </div>
-          </div>
-          <div className="Personalform-btn">
-            <Button
-              color="#fff"
-              bgColor="#007e99"
-              type="submit"
-              isLoading={methods.formState.isSubmitting}
-              mx="auto"
-              px="40px"
-              py="12px"
-              borderRadius="0"
-            >
-              Proceed
-            </Button>
-          </div>
-        </div>
-      </form>
-    </FormProvider>
-  );
+  try {
+   await axios.post(
+    "https://loanwise.onrender.com/api/borrowers-details",
+    values
+   );
+   handleNext();
+
+   reset();
+  } catch (error) {
+   if (error.response) {
+    setInValid(error.response.data.message);
+   } else {
+    console.error("Error while submitting form:", error.message);
+   }
+  }
+ };
+
+ return (
+  <FormProvider {...methods}>
+   {inValid && (
+    <span style={{ color: "red", marginBottom: "30px" }}>{inValid}</span>
+   )}
+   <form className="new-portfolio-form" onSubmit={handleSubmit(onSubmit)}>
+    <div className="new-portfolio-wrapper">
+     <h2>Personal Information</h2>
+     <div>
+      <NewFormInput
+       autoFocus={true}
+       type="text"
+       label="Full Name"
+       name="fullName"
+       placeholder="Enter full name"
+      />
+     </div>
+     <div>
+      <NewFormInput
+       type="text"
+       label="Residential Address"
+       name="address"
+       placeholder="Enter address"
+      />
+     </div>
+     <div>
+      <NewFormInput
+       name="email"
+       type="email"
+       label="Email Address"
+       placeholder="Enter email address"
+      />
+     </div>
+     <div className="dFlex">
+      <div className="dFlex1">
+       <NewFormInput
+        name="phoneNumber"
+        type="phone"
+        label="Phone Number"
+        placeholder="Enter number"
+       />
+      </div>
+      <div className="dFlex2">
+       <NewFormInput
+        name="alternatePhone"
+        type="phone"
+        label="Alternate Phone Number"
+        placeholder="Enter number"
+       />
+      </div>
+     </div>
+     <div className="dFlex">
+      <div className="dFlex1">
+       <NewFormInput name="dateOfBirth" type="date" label="Date of Birth" />
+      </div>
+      <div className="dFlex2">
+       <NewFormInput
+        name="bvn"
+        type="number"
+        label="Bank Verification Number"
+        placeholder="Enter number"
+       />
+      </div>
+     </div>
+     <div className="Personalform-btn">
+      <Button
+       color="#fff"
+       bgColor="#007e99"
+       type="submit"
+       isLoading={methods.formState.isSubmitting}
+       mx="auto"
+       px="40px"
+       py="12px"
+       borderRadius="0"
+      >
+       Proceed
+      </Button>
+     </div>
+    </div>
+   </form>
+  </FormProvider>
+ );
 };
 
 export default PersonalInfo;
--- src/Pages/Dashboard/Settings/Settingtoggle.jsx
+++ src/Pages/Dashboard/Settings/Settingtoggle.jsx
@@ -4,40 +4,35 @@ import arrow from "../../../Images/arrrow.svg";
 import { useNavigate } from "react-router-dom";
 
 const Settingtoggle = ({ header, subHeader, showItems, link }) => {
-  const [selectedIndex, setSelectedIndex] = useState(false);
-  const navigate = useNavigate();
+ const [selectedIndex, setSelectedIndex] = useState(false);
+ const navigate = useNavigate();
 
-  const handleToggle = () => {
-    if (link) {
-      //navigate to link
-      navigate(link);
-    } else {
-      setSelectedIndex(!selectedIndex);
-    }
-  };
+ const handleToggle = () => {
+  if (link) {
+   navigate(link);
+  } else {
+   setSelectedIndex(!selectedIndex);
+  }
+ };
 
-  return (
-    <div className="Container">
-      <div className="toggleContainer" onClick={handleToggle}>
-        <div className="toggle-title">
-          <h1>{header}</h1>
-          <p>{subHeader}</p>
-        </div>
-        {!link &&
-          (selectedIndex ? (
-            <img
-              src={arrow}
-              alt="arrow"
-              style={{ transform: "rotate(90deg)" }}
-            />
-          ) : (
-            <img src={arrow} alt="arrow" />
-          ))}
-      </div>
-
-      {selectedIndex && <div>{showItems}</div>}
+ return (
+  <div className="Container">
+   <div className="toggleContainer" onClick={handleToggle}>
+    <div className="toggle-title">
+     <h1>{header}</h1>
+     <p>{subHeader}</p>
     </div>
-  );
+    {!link &&
+     (selectedIndex ? (
+      <img src={arrow} alt="arrow" style={{ transform: "rotate(90deg)" }} />
+     ) : (
+      <img src={arrow} alt="arrow" />
+     ))}
+   </div>
+
+   {selectedIndex && <div>{showItems}</div>}
+  </div>
+ );
 };
 
 export default Settingtoggle;
